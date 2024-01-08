437bd493c566381da019430295b591e2bb816552
Nivrita44 <107574615+Nivrita44@users.noreply.github.com>
2024-01-09 00:41:33 +0600

    3

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tour/widgets/app_large_text.dart';
 import 'package:tour/widgets/app_text.dart';
+import 'package:tour/widgets/responsive_button.dart';
 
 import '../misc/colors.dart';
 
@@ -38,22 +39,38 @@ class _WelcomePageState extends State<WelcomePage> {
              child:Container(
                margin: const EdgeInsets.only(top:150, left:20,right:20),
                child:Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceBetween ,
                  children: [
                    Column(
                      crossAxisAlignment: CrossAxisAlignment.start,
                      children:[
                        AppLargeText(text: "Trips"),
                        AppText(text: "Mountain",size:30,),
-
+                       SizedBox(height: 20,),
                        Container(
                          width:250,
                          child:AppText(
                            text:"Mountain hikes give you an incredible sense of freedom along with endurance test ",
-                           color:AppColors.textColor2,
+                           color:AppColors.bigTextColor,
                            size:16,
                          ),
-                       )
+                       ),
+                       SizedBox(height: 40,),
+                       ResponsiveButton(width: 120,)
                      ],
+                   ),
+                   Column(
+                     children: List.generate(3, (indexDots){
+                       return Container(
+                         margin: const EdgeInsets.only(bottom: 2),
+                         width: 10,
+                         height: index==indexDots?25:8,
+                         decoration: BoxDecoration(
+                           borderRadius: BorderRadius.circular(8),
+                           color: index==indexDots?AppColors.mainColor:AppColors.mainColor.withOpacity(0.3)
+                         ),
+                       );
+                     }),
                    )
                    ],
                )
